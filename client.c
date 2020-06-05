abc0776340d8dfb13ad5931661c18c8322cc3168
Heedong Kim <59307414+ruthetum@users.noreply.github.com>
2020-06-05 23:29:10 +0900

    시간 측정

--- client.c
+++ client.c
@@ -4,11 +4,12 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <time.h>
 
-#define BUFFER_SIZE 1460
+#define BUFFER_SIZE 1024
 #define MAX_DIGIT 15
 
-void error_handling(char *msg);
+void error_handling(char *msg); // function for error handling
 
 int main(int argc, char *argv[]) {
   
@@ -39,7 +40,7 @@ int main(int argc, char *argv[]) {
   if (sock == -1)
     error_handling("socket() Error");
   
-  // Init
+  // Init (address,port)
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
@@ -51,19 +52,19 @@ int main(int argc, char *argv[]) {
   else
     puts("Connected...");
   
-  // Send 'Hello Server' to server
+  // Send message 'Hello Server' to the server
   write(sock, hsv, strlen(hsv));
   
-  // Receive a size of file from server
+  // Receive a size of file from the server
   read(sock, str_file_size, MAX_DIGIT);
   printf("Packet received.\n");
   printf("File size : %s\n", str_file_size);
   file_size = atoi(str_file_size); // string -> integer
   
-  // Open target
-  writeFile = fopen("received.mp4", "wb");
-  
+  // Measure working time
+  clock_t start = clock();
   // Read and Write
+  writeFile = fopen("rece.mp4", "wb");
   while ((recv_size = recv(sock, buff, BUFFER_SIZE, 0)) != 0) {
     fwrite(buff, sizeof(char), recv_size, writeFile);
     cur_size += recv_size;
@@ -72,16 +73,21 @@ int main(int argc, char *argv[]) {
     if (index % 100 == 0)
       printf("Progressing : %d / %d\n", cur_size, file_size);
   }
+  clock_t end = clock();
   
   // Close
   fclose(writeFile);
   close(sock);
   
+  printf("\n");
   // Compare size
   if (file_size == cur_size)
     printf("File Successfully Received! : %d / %d\n", cur_size, file_size);
   else
     printf("Failed Received... : %d / %d\n", cur_size, file_size);
+  
+  printf("Working Time : %lf sec\n", (double)(end - start)/CLOCKS_PER_SEC);
+  
   return 0;
 }
 
